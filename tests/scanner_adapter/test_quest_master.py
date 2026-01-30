2a9732c77d436467108967bd3d094050d24364e9
marek.borecki <marek.borecki@fasolutions.com>
2026-01-30 12:55:51 +0200

    adjust tests

--- tests/scanner_adapter/test_quest_master.py
+++ tests/scanner_adapter/test_quest_master.py
@@ -1,21 +1,39 @@
-from src.scan_adapter.scanner import is_reward_available
+from src.scan_adapter.scanner_adapter import Scanner
 from tests.scanner_adapter.html_utils import HtmlUtils
 
 
 def test_is_reward_available_with_reward():
+    # Given
     html = HtmlUtils.load("quest_master_with_reward.html")
+    scanner = Scanner()
 
-    assert is_reward_available(html) is True
+    # When
+    available = scanner.is_reward_available(html)
+
+    # Then
+    assert available is True
 
 
 def test_is_reward_not_available_without_reward():
+    # Given
     html = HtmlUtils.load("quest_master_without_reward.html")
+    scanner = Scanner()
 
-    assert is_reward_available(html) is False
+    # When
+    available = scanner.is_reward_available(html)
+
+    # Then
+    assert available is False
 
 
 # keep previous negative test to ensure hero_attributes page still returns False
 def test_is_reward_not_available_on_hero_attributes():
+    # Given
     html = HtmlUtils.load("hero_attributes.html")
+    scanner = Scanner()
 
-    assert is_reward_available(html) is False
+    # When
+    available = scanner.is_reward_available(html)
+
+    # Then
+    assert available is False
