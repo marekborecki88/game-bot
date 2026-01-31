3fc2774023ba2ebbab9156264d8777afddf2baef
marek.borecki <marek.borecki@fasolutions.com>
2026-01-31 14:00:07 +0200

    fix issue with collecting quest master rewards

--- src/core/task/tasks.py
+++ src/core/task/tasks.py
@@ -201,23 +201,22 @@ class CollectQuestmasterTask(Task):
             driver.wait_for_selector_and_click('#questmasterButton')
 
             # Click all 'Collect' controls
-            collect_selectors = [
-                "button:has-text('Collect')",
-                "button:has-text('collect')",
-                "a:has-text('Collect')",
-                "a:has-text('collect')",
-                "text=Collect",
-                "text=collect",
-            ]
+            collect_selectors = ["button:has-text('Collect')"]
 
             clicks = 0
             # iterate through reward pages until forward button is disabled
             while True:
+                driver.wait_for_selector("button:has-text('Collect')")
                 clicks += driver.click_all(collect_selectors)
+                # sleep 1 second to allow page to update
+                driver.sleep(1)
                 classes = driver.catch_full_classes_by_selector("button.forward")
-                if "disabled" in classes:
-                    break
-                driver.click("button.forward")
+                if "disabled" not in classes:
+                    driver.click("button.forward")
+                    continue
+
+                break
+
 
             # click general tasks
             driver.wait_for_selector_and_click("a.tabItem:has-text('General tasks')")
